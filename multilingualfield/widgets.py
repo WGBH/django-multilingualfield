bfe34a7cc472e26f6e9456a73eff03da9c9b103e
Jonathan Ellenberger <jonathan_ellenberger@wgbh.org>
2013-05-06 18:24:38 -0400

    Cleaning-up; adding custom Textarea widget
    Custom Textarea widget prepends a label with the languages verbose name.

--- multilingualfield/widgets.py
+++ multilingualfield/widgets.py
@@ -1,25 +1,49 @@
 from django.forms.widgets import (
     MultiWidget,
-    Textarea
+    Textarea,
+    TextInput
 )
+from django.utils.safestring import mark_safe
 
 from lxml import objectify
 from lxml.etree import XMLSyntaxError
 
 from multilingualfield import LANGUAGES
 
-class MultiLingualTextWidget(MultiWidget):
+class TextareaWithLabel(Textarea):
+    """
+    A form widget which prepends a <label> tag
+    """
+
+    def __init__(self, language, attrs):
+        self.label = language[1]
+        self.language_code = language[0]
+        super(TextareaWithLabel, self).__init__(attrs)
+
+    def render(self, name, value, attrs=None):
+        widget = super(TextareaWithLabel, self).render(name, value, attrs)
+        widget = mark_safe(
+                    "<div><label>%s</label>%s</div>" % (
+                                                        self.label,
+                                                        widget
+                                                    )
+                )
+        return widget
+
+class MultiLingualTextFieldWidget(MultiWidget):
     """
     A widget that returns a `Textarea` widget for each language specified
     in settings.LANGUAGES
     """
+    for_each_field_widget = TextareaWithLabel
+
     def __init__(self, attrs=None):
         self.languages = LANGUAGES
         widgets = [
-            Textarea()
-            for language_code, language_verbose in self.languages
+            self.for_each_field_widget(language, attrs)
+            for language in self.languages
         ]
-        super(MultiLingualTextWidget, self).__init__(widgets=widgets)
+        super(MultiLingualTextFieldWidget, self).__init__(widgets, attrs)
 
     def decompress(self, value):
         """
@@ -43,4 +67,4 @@ class MultiLingualTextWidget(MultiWidget):
             language_text_as_dict[language_code]
             if language_code in language_text_as_dict else ""
             for language_code, language_verbose in self.languages
-        ] 
+        ]

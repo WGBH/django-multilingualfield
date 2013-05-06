11c3ea49f00dfdfd59e978953a887a87b55f2460
Jonathan Ellenberger <jonathan_ellenberger@wgbh.org>
2013-05-06 18:28:08 -0400

    First pass at MultiLingualTextField
    Ensuring south introspection rules work
    Integrating form

---
+++ multilingualfield/fields.py
@@ -0,0 +1,43 @@
+from django.db.models import (
+    SubfieldBase,
+    TextField
+)
+
+from south.modelsinspector import add_introspection_rules
+
+from multilingualfield import LANGUAGES
+from multilingualfield.forms import (
+    MultiLingualTextForm
+)
+
+add_introspection_rules(
+    [],
+    [
+        "^multilingualfield\.fields\.MultiLingualTextField",
+        "^multilingualfield\.fields\.MultiLingualCharField",
+    ]
+)
+
+class MultiLingualTextField(TextField):
+    """
+    A django field for storing multiple manually-written
+    translations of the same piece of text.
+    """
+    description = "Stores multiple manually-written translations of the same piece of text."
+
+    __metaclass__ = SubfieldBase
+
+    def formfield(self, **kwargs):
+        # This is a fairly standard way to set up some defaults
+        # while letting the caller override them.
+        defaults = {'form_class': MultiLingualTextForm}
+        defaults.update(kwargs)
+        return super(MultiLingualTextField, self).formfield(**defaults)
+
+class MultiLingualCharField(MultiLingualTextField):
+    def formfield(self, **kwargs):
+        # This is a fairly standard way to set up some defaults
+        # while letting the caller override them.
+        defaults = {'form_class': MultiLingualTextForm}
+        defaults.update(kwargs)
+        return super(MultiLingualTextField, self).formfield(**defaults)

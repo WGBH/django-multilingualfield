ee00a377bea0660e31a9ff36e97bdc4ff0633536
Jonathan Ellenberger <jonathan_ellenberger@wgbh.org>
2013-05-07 09:57:46 -0400

    Tweaking ImproperlyConfigured exception message.

--- multilingualfield/__init__.py
+++ multilingualfield/__init__.py
@@ -8,4 +8,4 @@ LANGUAGES = getattr(settings, 'LANGUAGES', None)
 TEST_XML_FILE_LOCATION = open(os.path.join(APP_DIR, 'multilingual-test-values.xml'), 'r')
 
 if not LANGUAGES:
-    raise ImproperlyConfigured('The `multilingualfield` app requires that `LANGUAGES` must be set in your settings file.')
+    raise ImproperlyConfigured('The `multilingualfield` app requires that `LANGUAGES` (https://docs.djangoproject.com/en/dev/ref/settings/#languages) be set in your settings file.')

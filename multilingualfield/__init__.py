c975a408b58013169ed12603bfe935fba526c3ee
Jonathan Ellenberger <jonathan_ellenberger@wgbh.org>
2013-05-08 16:17:22 -0400

    Removing references to XML file used in development.

--- multilingualfield/__init__.py
+++ multilingualfield/__init__.py
@@ -3,9 +3,7 @@ import os
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-APP_DIR = os.path.abspath(os.path.dirname(__file__))
 LANGUAGES = getattr(settings, 'LANGUAGES', None)
-TEST_XML_FILE_LOCATION = open(os.path.join(APP_DIR, 'multilingual-test-values.xml'), 'r')
 
 if not LANGUAGES:
     raise ImproperlyConfigured('The `multilingualfield` app requires that `LANGUAGES` (https://docs.djangoproject.com/en/dev/ref/settings/#languages) be set in your settings file.')

b446aa57ece6152c958197c774765f830eb16aba
Jonathan Ellenberger <jonathan_ellenberger@wgbh.org>
2013-05-08 16:13:52 -0400

    Removing duplicate packages argument.

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     version='0.1',
     author=u'Jonathan Ellenberger',
     author_email='jonathan_ellenberger@wgbh.org',
-    packages=['multilingualfield'],
     url='http://github.com/WGBH/django-multilingualfield/',
     license='MIT License, see LICENSE',
     description='A django field for storing multiple manually-written' + \

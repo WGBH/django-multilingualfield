1289348e7e4f86fe7a85364f30d2c4efc2bbfdb5
Jonathan Ellenberger <jonathan_ellenberger@wgbh.org>
2013-05-08 16:11:50 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-multilingualfield',
+    packages=find_packages(),
+    version='0.1',
+    author=u'Jonathan Ellenberger',
+    author_email='jonathan_ellenberger@wgbh.org',
+    packages=['multilingualfield'],
+    url='http://github.com/WGBH/django-multilingualfield/',
+    license='MIT License, see LICENSE',
+    description='A django field for storing multiple manually-written' + \
+                'translations of the same piece of text.'
+    long_description=open('README.md').read(),
+    zip_safe=False,
+)

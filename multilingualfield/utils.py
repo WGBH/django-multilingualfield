4128a5f699f7a04eb15c9a4d099e551a90fdcd9c
Jonathan Ellenberger <jonathan_ellenberger@wgbh.org>
2014-02-05 15:39:25 -0500

    Another python 2.6 compatible dict constructor conversion

--- multilingualfield/utils.py
+++ multilingualfield/utils.py
@@ -35,7 +35,10 @@ def construct_MultiLingualText_from_xml(xml, instance):
         # with the language code (i.e. 'en', 'de', 'fr') as the key
         text_dict = {}
         try:
-            text_dict = {unicode(l.get(u'code')): unicode(l.text or u'') for l in xml_as_python_object.language}
+            text_dict = dict(
+                (unicode(l.get(u'code')), unicode(l.text or u''))
+                for l in xml_as_python_object.language
+            )
         except AttributeError:
             # Empty fields throw-off lxml and cause an AttributeError
             pass
